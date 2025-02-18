6fab5ff2a1f8895ca2fd730094621a98baa8caf6
shubham-medscribe <subham@medscribe.in>
2025-02-18 16:04:29 +0530

    fourth commit

--- src/components/reviewSlider/reviewSlider.tsx
+++ src/components/reviewSlider/reviewSlider.tsx
@@ -7,6 +7,45 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 export default function ReviewSlider() {
+  const reviews = [
+    {
+      name: "Farzaneh Zafari",
+      star: 5,
+      review:
+        "I truly appreciated Dr. Jain and Dr. Gupta and the amazing staff. They were also kind and made me feel comfortable throughout the process. Thanks to them. I now have a beautiful smile and I could be happier. I will definitely recommend this office to my friends and family and they have an in-house lab and amazing technology.",
+    },
+    {
+      name: "Michele",
+      star: 5,
+      review:
+        "I am very pleased with the care at the center for implant dentistry. I had full mouth implants, upper and lower jaw. I was given back my smile and the confidence in interaction with people. I’m very impressed with the technology here, it helped with the time. The staff is very knowledgeable and helpful, always ready to help and go the extra mile. I would recommend the center to my family and friends.",
+    },
+    {
+      name: "Mercedes Deguzman",
+      star: 5,
+      review:
+        "I've been going to this dental office for many years. Dr. Gupta and Dr. Jain and their staff have taken care of my dental needs exceptionally. They are experts in what they do. They did implants that eventually saved me grief from root canals, etc. It has made my life less stressful. I highly recommend them.",
+    },
+    {
+      name: "Chai Zhen Liao",
+      star: 5,
+      review:
+        "I had my upper jaw implant procedure done here and I couldn't be happier with the result. The team was professional, caring, and made the entire process smooth and comfortable, and Dr. Jain is very helpful. They did my job very well.",
+    },
+    {
+      name: "Minnie Novak",
+      star: 5,
+      review:
+        "I have been coming to Center for Implants for 6 years. I’m always treated with professionalism and courtesy. Staff is great! I have not ever had a problem with any of my treatments and visits.",
+    },
+    {
+      name: "Chieng Nim",
+      star: 5,
+      review:
+        "Getting the all on 8 full mouth implants at the center for implant dentistry has truly changed my life. I had implants on both jaws. Dr. Jain and Dr. Gupta are amazing. It's a big difference now. I can bite the whole apple without any problems. Great job with the staff and doctors.",
+    },
+  ];
+
   return (
     <div className="w-full flex flex-col items-center justify-center py-10">
       <div className="text-xl flex flex-col items-center gap-2 py-5">
@@ -33,51 +72,37 @@ export default function ReviewSlider() {
           }}
           className="rounded-lg shadow-lg space-x-2"
         >
-          <SwiperSlide>
-            <Card />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Card />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Card />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Card />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Card />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Card />
-          </SwiperSlide>
+          {reviews.map((review) => (
+            <SwiperSlide>
+              <Card
+                name={review.name}
+                review={review.review}
+                star={review.star}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
   );
 }
 
-function Card() {
+function Card({ name, review, star }: Record<string, any>) {
   return (
     <div className="w-full  flex flex-col p-5 bg-[#F9FAFF]">
       <div className="space-y-3">
         <div className="w-10 h-10 rounded-full bg-green-700 flex items-center justify-center text-white">
-          J
+          {name[0]}
         </div>
         <div className="flex gap-3">
-          <span>Paul N.</span>
+          <span>{name}</span>
           <span>
             <FontAwesomeIcon className="text-[#FF8754]" icon={faStar} />
           </span>
-          <span>5</span>
+          <span>{star}</span>
         </div>
         <div>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non
-            repudiandae atque saepe suscipit magni dolore, asperiores nesciunt
-            delectus corrupti veritatis! Deserunt voluptas beatae saepe a omnis
-            facere adipisci? Consectetur, est!
-          </p>
+          <p>{review}</p>
         </div>
       </div>
     </div>
